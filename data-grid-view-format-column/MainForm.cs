431a931b4e3e01566d0f748ce40fab5528b17633
Tom Gregor <tgregor@ivsoftware.com>
2022-12-26 16:38:51 -0700

    show full res on edit

--- data-grid-view-format-column/MainForm.cs
+++ data-grid-view-format-column/MainForm.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace data_grid_view_format_column
 {
@@ -14,12 +15,54 @@ namespace data_grid_view_format_column
             {
                 column.DefaultCellStyle.Format = "F2";
             }
+            dataGridView.EditingControlShowing += (sender, e) =>
+            {
+                if(dataGridView.CurrentCell.Value is decimal d)
+                {
+                    dataGridView.EditingControl.Text = $"{d}";
+                }
+            };
         }
+
         BindingList<MyRow> Rows = new BindingList<MyRow>();
     }
-    class MyRow
-    { 
-        public decimal ValueA { get; set; }
-        public decimal ValueB { get; set; }
+    class MyRow : INotifyPropertyChanged
+    {
+        decimal _ValueA = 0;
+        public decimal ValueA
+        {
+            get => _ValueA;
+            set
+            {
+                if (!Equals(_ValueA, value))
+                {
+                    _ValueA = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        decimal _ValueB = 0;
+        public decimal ValueB
+        {
+            get => _ValueB;
+            set
+            {
+                if (!Equals(_ValueB, value))
+                {
+                    _ValueB = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
     }
 }
