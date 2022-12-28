44675d50d7570b1d3854d76b2cb56779270b4e19
Tom Gregor <tgregor@ivsoftware.com>
2022-12-28 15:25:03 -0700

    copy prelim OK

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ![cell-with-hover](https://github.com/IVSoftware/data-grid-view-format-column-00/blob/master/data-grid-view-format-column/Screenshots/cell-with-hover.png)
 
+***
     public partial class MainForm : Form
     {
         public MainForm() => InitializeComponent();
--- data-grid-view-format-column/MainForm.cs
+++ data-grid-view-format-column/MainForm.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace data_grid_view_format_column
 {
@@ -15,14 +16,45 @@ namespace data_grid_view_format_column
             {
                 column.DefaultCellStyle.Format = "F2";
             }
+            // Show full resolution in editing control.
             dataGridView.EditingControlShowing += (sender, e) =>
             {
-                if(dataGridView.CurrentCell.Value is decimal d)
+                var value = dataGridView.CurrentCell.Value;
+                if (value != null)
                 {
-                    dataGridView.EditingControl.Text = $"{d}";
+                    dataGridView.EditingControl.Text = $"{value}";
+                }
+            };
+            dataGridView.KeyDown += (sender, e) =>
+            {
+                if(e.KeyData.Equals(Keys.Control | Keys.C ))
+                {
+                    e.SuppressKeyPress = e.Handled = true;
+                    copyAllToClipboard();
                 }
             };
         }
+
+        private void copyAllToClipboard()
+        {
+            List<string>
+                rowBuilder = new List<string>(),
+                columnBulder = new List<string>();
+
+            for (int row = 0; row < Rows.Count; row++)
+            {
+                for (int col = 0; col < dataGridView.ColumnCount; col++)
+                {
+                    var cell = dataGridView[col, row];
+                    columnBulder.Add(cell.Value == null ? string.Empty : $"{cell.Value}");
+                }
+                rowBuilder.Add(string.Join("\t", columnBulder));
+                columnBulder.Clear();
+            }
+            var csv = string.Join(Environment.NewLine, rowBuilder);
+            Clipboard.SetText(csv);
+        }
+
         BindingList<MyRow> Rows = new BindingList<MyRow>();
     }
     class MyRow
